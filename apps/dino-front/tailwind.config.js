f899c0d5ecaf871e1008dc76afaabc1e7968b647
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-06-07 14:35:18 +0900

    PRD-3 Feat :   ui , tailwind-config 추가

--- apps/dino-front/tailwind.config.js
+++ apps/dino-front/tailwind.config.js
@@ -3,6 +3,5 @@ const sharedConfig = require('tailwind-config/tailwind.config.js');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // prefix ui lib classes to avoid conflicting with the app
   presets: [sharedConfig],
 };
