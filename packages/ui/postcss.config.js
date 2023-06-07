fd6a686cf5a327d608f156c5f2afe5fc87ab1926
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-06-07 11:30:25 +0900

    PRD-3-2 Feat : tailwind plugin 설정

--- packages/ui/postcss.config.js
+++ packages/ui/postcss.config.js
@@ -1,6 +1,13 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
 module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
+    },
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
-}
+};
--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,9 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const sharedConfig = require('tailwind-config/tailwind.config.js');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+  presets: [sharedConfig],
+};
