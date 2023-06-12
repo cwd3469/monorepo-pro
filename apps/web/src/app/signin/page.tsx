f6bba389ebd85da8c58e967f9d5d84b4135e5303
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-06-12 14:19:03 +0900

    PRD-2 Feat : hospital add

--- apps/web/src/app/signin/page.tsx
+++ apps/web/src/app/signin/page.tsx
@@ -1,7 +1,19 @@
 import { WSigninContainer } from 'ui';
+import Image from 'next/image';
 
 const SigninPage = () => {
-  return <WSigninContainer />;
+  return (
+    <WSigninContainer>
+      <>
+        <Image
+          src={''}
+          alt={'public/images/signin_hospital_logo'}
+          width="300"
+          height="300"
+        />
+      </>
+    </WSigninContainer>
+  );
 };
 
 export default SigninPage;
