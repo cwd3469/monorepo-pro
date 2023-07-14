3327af9d75f83f9ceea0f281f871fcaba76847b3
jooyoung-choi-bepluslab <cwd3469@bepluslab.co.kr>
2023-07-14 13:43:51 +0900

    PRD-2 Feat : tailwind.config 변경

--- apps/wau-hospital/tailwind.config.js
+++ apps/wau-hospital/tailwind.config.js
@@ -4,9 +4,9 @@ const sharedConfig = require('tailwind-config');
 module.exports = {
   ...sharedConfig,
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/container/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/presentational/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     '../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
 };
